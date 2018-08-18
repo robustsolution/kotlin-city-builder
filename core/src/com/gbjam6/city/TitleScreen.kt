e178dde7107d68627b75097d0ab2d14f7718b90b
bluegeniusdev <yopoxdev@gmail.com>
2018-08-18 16:10:49 +0900

    Added "game logo" sprite

--- core/src/com/gbjam6/city/TitleScreen.kt
+++ core/src/com/gbjam6/city/TitleScreen.kt
@@ -4,37 +4,46 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.app.KtxScreen
 
 class TitleScreen(gbJam6: GBJam6): KtxScreen {
 
-    private val stage = Stage()
+    private val batch = SpriteBatch()
     private val camera = OrthographicCamera()
     private val viewport = FitViewport(160f, 144f, camera)
+
     private val bgColor = Color.valueOf("cdf6f7")
+    private val titleName = Sprite(Texture("name.png"))
 
     override fun show() {
         super.show()
-        Gdx.input.inputProcessor = stage
     }
 
     override fun render(delta: Float) {
 
         camera.update()
-        stage.act(Gdx.graphics.deltaTime);
 
         // Clear screen
         Gdx.gl.glClearColor(bgColor.r, bgColor.g, bgColor.b,  1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        stage.draw()
+        // Prepare for drawing
+        batch.projectionMatrix = camera.combined
+        batch.begin()
+
+        batch.draw(titleName, -64f, 16f)
+
+        batch.end()
 
     }
 
     override fun dispose() {
-        stage.dispose()
+        batch.dispose()
     }
 
     override fun resize(width: Int, height: Int) {
