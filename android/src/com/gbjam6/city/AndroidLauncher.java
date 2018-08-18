a42e3c71f6959eb0273381df23972ad2d98ab013
bluegeniusdev <yopoxdev@gmail.com>
2018-08-18 14:24:45 +0900

    Initial commit

---
+++ android/src/com/gbjam6/city/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gbjam6.city;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gbjam6.city.GBJam6;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GBJam6(), config);
+	}
+}
---
+++ desktop/src/com/gbjam6/city/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gbjam6.city.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gbjam6.city.GBJam6;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GBJam6(), config);
+	}
+}
