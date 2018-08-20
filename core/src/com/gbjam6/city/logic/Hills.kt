8c29e0353fb4a07bd12e01cc38509bc688bcd578
bluegeniusdev <42486148+Mirionos@users.noreply.github.com>
2018-08-20 14:53:46 +0900

    Hills Enhance

--- core/src/com/gbjam6/city/logic/Hills.kt
+++ core/src/com/gbjam6/city/logic/Hills.kt
@@ -9,31 +9,35 @@ class Hills {
     val chunks: List<Chunk>
 
     companion object {
-        val slopes = listOf(-16, -16, -8, -8, -8, -8, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 16, 16)
-        val heights = listOf(16, 24, 32, 40, 48, 56, 64)
+        val slopes = listOf(-16, -8, -8, -8, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 16)
+        val heights = listOf(16, 24, 32, 40, 48)
     }
 
     init {
         val chunksTemp = mutableListOf<Chunk>()
-        chunksTemp.add(Chunk(heights.elementAt(Random().nextInt(heights.size)), 0))
-        for (i in 0..48) {
-            chunksTemp.add(chunkChoice(chunksTemp.elementAt(i)))
+        val n = Random().nextInt(heights.size)
+        chunksTemp.add(Chunk(heights.elementAt(n), 0))
+        chunksTemp.add(Chunk(heights.elementAt(n), 0))
+        for (i in 1..48) {
+            chunksTemp.add(chunkChoice(chunksTemp.elementAt(i - 1), chunksTemp.elementAt(i)))
         }
         chunks = chunksTemp.toList()
     }
 
-    private fun chunkChoice(preChunk: Chunk): Chunk {
+    private fun chunkChoice(prepreChunk: Chunk, preChunk: Chunk): Chunk {
         val height = preChunk.height + preChunk.slope
-        val slopes2 = when {
-            height in 33..47 -> slopes
-            height in 48..63 -> slopes.filter { it < 16 }
-            height >= 64 -> slopes.filter { it < 8 }
-            height in 17..32 -> slopes.filter { it > -16 }
-            else -> slopes.filter { it > -8 }
-        }
-        var slope = slopes2.elementAt(Random().nextInt(slopes2.size))
-        if (preChunk.slope < 0 && slope > 0 || preChunk.slope > 0 && slope < 0) {
-            slope = 0
+        val slope: Int
+        slope = if (prepreChunk.slope == 0 && preChunk.slope == 0) {
+            val slopes2 = when {
+                height in 33..47 -> slopes.filter { it < 16 }
+                height >= 48 -> slopes.filter { it < 8 }
+                height == 32 -> slopes
+                height in 17..31 -> slopes.filter { it > -16 }
+                else -> slopes.filter { it > -8 }
+            }
+            slopes2.elementAt(Random().nextInt(slopes2.size))
+        } else {
+            0
         }
         return Chunk(height, slope)
     }
