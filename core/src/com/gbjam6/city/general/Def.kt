9e80e23b38324b69156b3366c6e33c6aa0c72088
bluegeniusdev <42486148+Mirionos@users.noreply.github.com>
2018-08-25 07:03:02 +0900

    Game Design Change

--- core/src/com/gbjam6/city/general/Def.kt
+++ core/src/com/gbjam6/city/general/Def.kt
@@ -87,7 +87,7 @@ object Def {
     // BUILDINGS
     val buildings = listOf(
             LBuilding(BuildingType.CITIZENS, "HOUSE", 6,
-                    Pair(34, 41), Pair(34, 41), Pair(24, 41), 100),
+                    Pair(34, 41), Pair(34, 41), Pair(24, 41), 100,upgradeCost = 100),
             LBuilding(BuildingType.HAPPINESS, "TAVERN", 2,
                     Pair(13, 20), Pair(13, 26), Pair(13, 39), 100),
             LBuilding(BuildingType.FOOD, "FARM", 2,
@@ -95,7 +95,7 @@ object Def {
             LBuilding(BuildingType.RESEARCH, "LABORATORY", 2,
                     Pair(20, 28), Pair(20, 29), Pair(20, 38), 100),
             LBuilding(BuildingType.STONE, "FACTORY", 2,
-                    Pair(6, 19), Pair(6, 30), Pair(6, 22), 100),
+                    Pair(6, 19), Pair(6, 30), Pair(6, 22), 100,upgradeCost = 100),
             LBuilding(BuildingType.OTHER, "WELL", 0,
                     Pair(0, 17), Pair(0, 17), Pair(0, 17), 100),
             LBuilding(BuildingType.STONE, "CRAFTMAN", 1,
